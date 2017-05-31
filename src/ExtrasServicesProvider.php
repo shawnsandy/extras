332bfc89bc235c652852f601d23f1f03f7b962b0
shawn sandy <shawnsandy04@gmail.com>
2017-05-31 16:01:30 -0400

    WIP - update the class names on services providers

--- src/ExtrasServicesProvider.php
+++ src/ExtrasServicesProvider.php
@@ -49,7 +49,7 @@
              * Package config
              */
             $this->publishes(
-                [__DIR__ . '/config/config.php' => config_path('extras.php')],
+                [__DIR__ . '/config/config.php' => config_path('extras/settings.php')],
                 'extras-config'
             );
 
@@ -106,9 +106,9 @@
              * load service providers
              */
 
-            $this->app->register('davestewart\sketchpad\SketchpadServiceProvider');
-            $this->app->register('Collective\Remote\RemoteServiceProvider');
-            $this->app->register('Collective\Html\HtmlServiceProvider');
+            $this->app->register(\davestewart\sketchpad\SketchpadServiceProvider::class);
+            $this->app->register(\Collective\Remote\RemoteServiceProvider::class);
+            $this->app->register(\Collective\Html\HtmlServiceProvider::class);
             $this->app->register('Mews\Purifier\PurifierServiceProvider');
             $this->app->register('Thujohn\Twitter\TwitterServiceProvider');
             $this->app->register('Brotzka\DotenvEditor\DotenvEditorServiceProvider');
@@ -121,8 +121,8 @@
              * Load aliases / facades
              */
 
-            $aliases->alias("Html", 'Collective\Html\HtmlFacade');
-            $aliases->alias("Form", 'Collective\Html\FormFacade');
+            $aliases->alias("Html", \Collective\Html\HtmlFacade::class);
+            $aliases->alias("Form", \Collective\Html\FormFacade::class);
             $aliases->alias('Purifier', 'Mews\Purifier\Facades\Purifier::class');
             $aliases->alias("Twitter", 'Thujohn\Twitter\Facades\Twitter::class');
             $aliases->alias('DotenvEditor', 'Brotzka\DotenvEditor\DotenvEditorFacade');
@@ -149,101 +149,3 @@
 
 
     }
-
-namespace ShawnSandy\Extras;
-
-use Abraham\TwitterOAuth\TwitterOAuth;
-use Illuminate\Support\ServiceProvider;
-use ShawnSandy\Extras\Apps\Maps\Maps;
-use ShawnSandy\Extras\Apps\Socialize\Socializer;
-use ShawnSandy\Extras\Apps\Socialize\Twitter\TwitterAuth;
-
-/**
- * Class Provider
- *
- * @package ShawnSandy\PkgStart
- */
-class ExtrasServicesProvider extends ServiceProvider
-{
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        if (!$this->app->routesAreCached()) {
-            include __DIR__ . '/routes.php';
-        }
-
-        /**
-         * Package views
-         */
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'extras');
-        $this->publishes(
-            [
-                __DIR__ . '/resources/views' => resource_path('views/vendor/extras'),
-            ], 'extras-views'
-        );
-
-        /**
-         * Package assets
-         */
-        $this->publishes(
-            [
-                __DIR__ . '/resources/assets/js/' => public_path('assets/extras/js/'),
-                __DIR__ . '/public/assets/' => public_path('assets/')
-            ], 'extras-assets'
-        );
-
-        /**
-         * Package config
-         */
-        $this->publishes(
-            [__DIR__ . '/config/config.php' => config_path('extras.php')],
-            'extras-config'
-        );
-
-        if (!$this->app->runningInConsole()) :
-            include_once __DIR__ . '/Helpers/helper.php';
-        endif;
-
-        include_once __DIR__ . "/components/extras.php";
-
-
-    }
-
-    /**
-     * Register any package services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-        $this->mergeConfigFrom(
-            __DIR__ . '/config/config.php', 'extras'
-        );
-
-        $this->app->bind(
-            'Extras', function () {
-            return new Extras();
-        });
-
-        $this->app->bind(
-            'Gmap', function () {
-            return new Maps();
-        });
-
-        $this->app->bind('Socialize', function() {
-           return new Socializer();
-        });
-
-        $this->app->bind('twitterauth', function(){
-            return new TwitterAuth();
-        });
-
-
-    }
-
-}
