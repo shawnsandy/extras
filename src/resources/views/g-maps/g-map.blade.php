9d8a3260693b331763351430b22eec45a1da93de
shawn sandy <shawnsandy04@gmail.com>
2017-06-22 14:48:10 -0400

    Update - remove container wrapper from map frame

--- src/resources/views/g-maps/g-map.blade.php
+++ src/resources/views/g-maps/g-map.blade.php
@@ -2,7 +2,7 @@
     $map = Gmap::getMapData($address);
 @endphp
 
-<div class="row g-map">
+<div class="g-map">
     <div id="g-map" style="height: {{ $style["height"] or '700px' }}; {{ $style["width"] or '100%' }}"
          data-lat="{{ $map["lat"] }}" data-lng="{{ $map["lng"] }}" data-zoom="{{ $options["zoom"] or '15'}}"
          data-scroll="{{ $options["scroll"] or 'true'}}"></div>
@@ -13,5 +13,4 @@
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key={{ config("extras.settings.keys.google_maps_api_key") }}&callback=initMap">
 </script>
-
 @endpush
