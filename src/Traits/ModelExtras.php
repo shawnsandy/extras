bec3b18024d5a1a9c49bf157b053d8e550770910
shawnsandy04 <shawnsandy04@gmail.com>
2017-06-23 04:49:03 -0400

    Feature - trait ModelExtras reuseable model functions

---
+++ src/Traits/ModelExtras.php
@@ -0,0 +1,55 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Shawn
+     * Date: 6/23/2017
+     * Time: 4:34 AM
+     */
+
+    namespace ShawnSandy\Extras\Traits;
+
+
+    trait ModelExtras
+    {
+
+        /**
+         * @param $query
+         * @return mixed
+         */
+        public function scopeLatest($query, $limit = 15)
+        {
+            return $query->orderBy("id", "DESC")->take($limit);
+        }
+
+        /**
+         * @param     $query
+         * @param int $paginate
+         * @return mixed
+         */
+        public function scopeLatestPaginated($query, $paginate = 20)
+        {
+            return $query->orderBy("id", "DESC")->paginate($paginate);
+        }
+
+        public function scopeAsc($query, $column = "id")
+        {
+            return $query->orderBy($column, "ASC");
+        }
+
+        public function scopeDesc($query, $column = "id")
+        {
+            return $query->orderBy($column, "ASC");
+        }
+
+
+        public function getCreatedSinceAttribute($query)
+        {
+            return $this->created_at->diffForHumans();
+        }
+
+        public function getUpdatedSinceAttribute($query)
+        {
+            return $this->updated_at->diffForHumans();
+        }
+
+    }
