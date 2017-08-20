438ea1ba657eea4b4bc7150539a3a721439959ff
shawnsandy04 <shawnsandy04@gmail.com>
2017-08-19 21:50:26 -0400

    update - remove data_* variables

--- src/resources/views/elements/data-table.blade.php
+++ src/resources/views/elements/data-table.blade.php
@@ -6,10 +6,6 @@
             return "<td class=\"{$item}\">" . ucwords(str_replace("_", " ", $item)) . "</td>";
         });
 
-    $button = '<button class="data-btn btn btn-primary btn-xs" disabled="true-" style="display: none-;">View / Edit</button>';
-    $columns_data = collect(collect($data)->first())->keys()->map(function($items) {
-        return ["data" => $items, "class" => $items];
-     })->push(["data" => null, "class" => 'action', 'defaultContent' => "$button"]);
 
     if(!isset($action_url))
    $action_url = url()->current()."/";
@@ -47,8 +43,7 @@
             src="https://cdn.datatables.net/v/bs/jq-2.2.4/dt-1.10.15/b-1.3.1/r-2.1.1/datatables.min.js"></script>
     <script>
 
-        var table_data = <?= collect($data) ?>;
-        var table_columns = <?= $columns_data ?>;
+
         var el = "{{ $table_id or "#data-tables"}}";
         var edit_url = "{{ $action_url }}";
 
